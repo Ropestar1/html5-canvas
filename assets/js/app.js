13667e86a5194386f890619c66bf11e328eabcd3
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-09-21 17:41:42 -0700

    Adding canvas event listener.

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+// creating a variable so we can indicate when drawing is occurring (like on a click and hold)
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+  console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
